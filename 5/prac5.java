3e0c2abf239c8f082c5b11da4f8d6fed97033e69
Lydell Dsouza <113416533+Lydell2627@users.noreply.github.com>
2025-05-06 09:36:49 +0530

    Update prac5.java removed comments

--- 5/prac5.java
+++ 5/prac5.java
@@ -17,12 +17,12 @@ public class prac5 {
          map.put(1, "Apple");
          map.put(4, "Date");
          map.put(2, "Banana");
-         // Sort the map by keys in ascending order using TreeMap
+
          Map<Integer, String> sortedMap = new TreeMap<>(map);
         
         
          Set<Entry<Integer, String>> s= sortedMap.entrySet();
-         // Display the sorted Map elements using normal for-each loop
+
          for (Entry<Integer, String> entry : s) {
         
          System.out.println(entry.getKey()+" : "+ entry.getValue());
